bd5b5d5ed7c045c64d65d8a9533cc77070312e7e
Kritika-eng <kritikagarg267@gmail.com>
2026-06-21 20:15:37 +0530

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,1 +1,16 @@
+name = "Kritika"
+city = "Muzaffarnagar"
+favorite_subject = "Literature"
+target_role = "Content Writer"
 
+student = {
+    "name": name,
+    "city": city,
+    "favorite_subject": favorite_subject,
+    "target_role": target_role
+}
+
+print(f"My name is {student['name']}.")
+print(f"I live in {student['city']}.")
+print(f"My favorite subject is {student['favorite_subject']}.")
+print(f"I want to become a {student['target_role']}.")
